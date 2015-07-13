36f58cc3c203cfe94b036e4f5e8bc0da36c60ae1
Eli Feasley <eliana@khanacademy.org>
2015-07-12 22:20:51 -0700

    stage jquery animations

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
-getRandomColor = function() {
-	return '#' + ('00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6);
-}
+var getRandomColor = function() {
+	return '#' + ('00000'+(Math.random()*(1 << 24)|0).toString(16)).slice(-6);
+};
 
 travelToNewColor = function(oldColor, newColor) {
-	diff1 = oldColor.slice - newColor.slice
+	diff1 = oldColor.slice - newColor.slice;
+};
 
-}
-
-var oldColor = getRandomColor()
+var oldColor = getRandomColor();
 window.addEventListener('click', function() {
+	$(".target")[0].removeEventListener("mousemove", function() {css({"background-color": getRandomColor()});});
 	$("body").css({"background-color": oldColor});
-	var newColor = getRandomColor();
-	$(".target").css({"background-color": newColor, "height": "200px", "width": "200px", "border-radius": "500px"})
-	$(".target").show();
-	$(".target").animate({'height':1000, "width":1000}, 1000);
 	oldColor = newColor;
-	$(".target").fadeOut(2000).delay(1000);
-	//$("body").animate({"background-color": newColor});
+	var newColor = getRandomColor();
+	$(".target").css(
+    {"background-color": newColor, 
+     "height": "200px", 
+     "width": "200px", 
+     "border-radius": "500px"});
+	$(".target").show();
+	$(".target").animate(
+		{'height':1000, "width":1000}, 1000,
+		function () {$(".target").fadeOut(2000).delay(1000, function() {
+			$(".target").css({"background-color": getRandomColor()});
+			$(".target").show();});});
+	$(".target")[0].addEventListener("mousemove", function() {$(".target").css({"background-color": getRandomColor()});});
+
 });
-	
- 
